802fb9f138510e11f796dd462b49f530583a22b2
Christoff Erasmus <ER8797@isservices.co.za>
2019-02-13 16:50:37 +0200

    FOR PHILLIP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import './index.css'
-import numeral from 'numeral';
+// import './index.css'
+import { getUsers } from './api/userApi.js';
 
-/* eslint-disable no-console */
+// Pupulate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
 
-// console.log('Beginning the index.js');
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+  console.log('The usersBody is: '+usersBody);
+  global.document.getElementById('users').innerHTML = usersBody;
+})
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
-
-// console.log('Leaving the index.js');
